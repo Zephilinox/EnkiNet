25a898ce29bc1eb5bfdf44b9c69eddc9093ac7ec
Zephilinox <zephilinox@hotmail.co.uk>
2018-10-05 14:32:05 +0100

    serialize/deserialize ints

--- src/networking/Packet.cpp
+++ src/networking/Packet.cpp
@@ -2,17 +2,33 @@
 
 Packet::Packet()
 	: bytes(1400)
+	, bytes_written(0)
+	, bytes_read(0)
 {
 }
 
 Packet& Packet::operator<<(int data)
 {
-	memcpy(bytes.data(), &data, sizeof(data));
+	serialize(&data);
 	return *this;
 }
 
 Packet& Packet::operator>>(int& data)
 {
-	memcpy(&data, bytes.data(), sizeof(data));
+	deserialize(&data);
 	return *this;
+}
+
+template <typename T>
+void Packet::serialize(T* data)
+{
+	memcpy(bytes.data() + bytes_written, data, sizeof(T));
+	bytes_written += sizeof(T);
+}
+
+template <typename T>
+void Packet::deserialize(T* data)
+{
+	memcpy(data, bytes.data() + bytes_read, sizeof(T));
+	bytes_read += sizeof(T);
 }
--- src/networking/Packet.hpp
+++ src/networking/Packet.hpp
@@ -13,5 +13,13 @@ public:
 	Packet& operator >>(int& data);
 
 private:
+	template <typename T>
+	void serialize(T* data);
+
+	template <typename T>
+	void deserialize(T* data);
+
 	std::vector<std::byte> bytes;
+	int bytes_written;
+	int bytes_read;
 };
